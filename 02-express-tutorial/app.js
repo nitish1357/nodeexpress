b73305a26c6bb3b80a9241a5455da4218801c6cc
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-17 08:06:40 +0530

    http-basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('User hit the server');
+  res.end('Home page');
+});
+
+server.listen(5000);
