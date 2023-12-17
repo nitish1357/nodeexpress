5d4d35e26ee6e7400aa6398808a670e09c6c9ee4
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-17 10:12:31 +0530

    manually set headers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -10,6 +10,13 @@ app.get('/about', (req, res) => {
   res.status(200).send('About page');
 });
 
+// To manually set the Content-Type header for res.send() responses, you can use the res.set() or res.setHeader() methods.
+app.get('/api/data', (req, res) => {
+  const data = 'Hello, world!';
+  res.set('Content-Type', 'text/plain');
+  res.send(data);
+});
+
 app.all('*', (req, res) => {
   res.status(404).send('<h1>Resource not found</h1>');
 });
