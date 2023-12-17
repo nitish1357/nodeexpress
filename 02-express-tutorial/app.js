420864212f21d79dda48439a95357ed02369898e
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-17 08:58:24 +0530

    http-App example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,12 +2,15 @@ const http = require('http');
 const { readFileSync } = require('fs');
 
 // get all files
-const homePage = readFileSync('./index.html');
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
-  //   console.log(req.method);
-  //   console.log(req.url);
+  // console.log(req.method)
   const url = req.url;
+  console.log(url);
   // home page
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
@@ -20,6 +23,24 @@ const server = http.createServer((req, res) => {
     res.write('<h1>about page</h1>');
     res.end();
   }
+  // styles
+  else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homeStyles);
+    res.end();
+  }
+  // image/logo
+  else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' });
+    res.write(homeImage);
+    res.end();
+  }
+  // logic
+  else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homeLogic);
+    res.end();
+  }
   // 404
   else {
     res.writeHead(404, { 'content-type': 'text/html' });
@@ -27,5 +48,24 @@ const server = http.createServer((req, res) => {
     res.end();
   }
 });
+
+// Switch statement syntax
+
+//   switch (url) {
+//     case '/':
+//       res.writeHead(200, { 'content-type': 'text/html' });
+//       res.write(homepage);
+//       res.end();
+//       break;
+//     case '/styles.css':
+//       // do something and stop comparing for other values
+//       break;
+//     default:
+//       // if nothing match, send 404 status (like final else)
+//       res.writeHead(404, { 'content-type': 'text/html' });
+//       res.write('<h1> Page is not found </h1>');
+//       res.end();
+//       break;
+//   }
 
 server.listen(5000);
