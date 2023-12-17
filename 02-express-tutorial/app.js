c131732f4b71e3be58019ca6a64c82749c28a1db
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-17 08:35:10 +0530

    http-requestobject

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  //   res.writeHead(200, { 'content-type': 'text/html' });
-  //   res.end('<h1>Home page</h1>');
-  res.writeHead(200, { 'content-type': 'text/html' });
-  //   res.writeHead(200, { 'content-type': 'text/plain' });
-  res.write('<h1>Home page</h1>');
-  res.end();
+  //   console.log(req.method);
+  //   console.log(req.url);
+  const url = req.url;
+  // home page
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>home page</h1>');
+    res.end();
+  }
+  // about page
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>about page</h1>');
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>page not found</h1>');
+    res.end();
+  }
 });
 
 server.listen(5000);
