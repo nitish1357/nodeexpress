9e929f15af9a17d3db5b77c74d4d741c593df5a5
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-17 10:58:02 +0530

    json-basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// setup static and middleware
-app.use(express.static('./public'));
+const { products } = require('./data');
 
 // app.get('/', (req, res) => {
-//   //   console.log(path.resolve(__dirname, './navbar-app/index.html'));
-//   //   console.log(path.join(__dirname, './navbar-app/index.html'));
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+//   res.json([{ name: 'John' }, { name: 'Danny' }]);
 // });
-
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Resource not found</h1>');
+app.get('/', (req, res) => {
+  res.json(products);
 });
 
 app.listen(5000, () => {
