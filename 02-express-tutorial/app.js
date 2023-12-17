83bf9b0add910f56bb346e0a866f532d42304590
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-17 10:04:38 +0530

    express basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,71 +1,19 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const app = express();
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
-
-const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url;
-  console.log(url);
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(homePage);
-    res.end();
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>about page</h1>');
-    res.end();
-  }
-  // styles
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' });
-    res.write(homeStyles);
-    res.end();
-  }
-  // image/logo
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' });
-    res.write(homeImage);
-    res.end();
-  }
-  // logic
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' });
-    res.write(homeLogic);
-    res.end();
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' });
-    res.write('<h1>page not found</h1>');
-    res.end();
-  }
+app.get('/', (req, res) => {
+  console.log('User hit the resource');
+  res.status(200).send('Home page');
 });
 
-// Switch statement syntax
+app.get('/about', (req, res) => {
+  res.status(200).send('About page');
+});
 
-//   switch (url) {
-//     case '/':
-//       res.writeHead(200, { 'content-type': 'text/html' });
-//       res.write(homepage);
-//       res.end();
-//       break;
-//     case '/styles.css':
-//       // do something and stop comparing for other values
-//       break;
-//     default:
-//       // if nothing match, send 404 status (like final else)
-//       res.writeHead(404, { 'content-type': 'text/html' });
-//       res.write('<h1> Page is not found </h1>');
-//       res.end();
-//       break;
-//   }
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Resource not found</h1>');
+});
 
-server.listen(5000);
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000');
+});
