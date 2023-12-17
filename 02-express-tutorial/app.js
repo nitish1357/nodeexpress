65820dd522257775660300be2df2266f8de30e2b
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-17 11:31:36 +0530

    send static data using json file

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const { products } = require('./data');
 
-// app.get('/', (req, res) => {
-//   res.json([{ name: 'John' }, { name: 'Danny' }]);
-// });
 app.get('/', (req, res) => {
-  res.json(products);
+  res.send('<h1>Home page</h1> <a href="/api/products">Products</a>');
+});
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  //   res.send(products);
+  res.send(newProducts);
 });
 
 app.listen(5000, () => {
