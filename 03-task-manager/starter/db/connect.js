d51a652f20aafaf77572b5630cf17147369fd366
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-18 22:44:20 +0530

    connect to DB

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,2 +1,14 @@
+const mongoose = require('mongoose');
+
 const connectionString =
   'mongodb+srv://nitish:basictomato@node-express.dfnlqxk.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to the DB...'))
+  .catch((err) => console.log(err));
