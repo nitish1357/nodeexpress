176827af23e7ba08c61ab9bbf3411a20b5b16426
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-18 21:52:24 +0530

    get all tasks

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
+
+// middleware
+app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
   res.send('Task manager app');
 });
+
+app.use('/api/v1/tasks', tasks);
 
 // app.get('/api/v1/tasks')   - get all the tasks
 // app.post('/api/v1/tasks')   - create a new task
@@ -12,6 +18,6 @@ app.get('/', (req, res) => {
 // app.patch('/api/v1/tasks/:id')   -update task
 // app.delete('/api/v1/tasks/:id')   -delete task
 
-const port = 5000;
+const port = 3000;
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req, res) => {
+  res.send('Get all items');
+};
+
+module.exports = {
+  getAllTasks,
+};
