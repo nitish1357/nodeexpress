e66bcb44848c9fa030eb197b81bc4f55952da83a
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-18 22:54:44 +0530

    Refactor connect

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const connectionString =
   'mongodb+srv://nitish:basictomato@node-express.dfnlqxk.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to the DB...'))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
