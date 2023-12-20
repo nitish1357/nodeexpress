9eb3fc52fcc2162588057fa369627536107a3c08
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-20 06:06:26 +0530

    response types

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -4,6 +4,13 @@ const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
+    // res.status(200).json({ tasks, amount: tasks.length });
+    // res
+    //   .status(200)
+    //   .json({ success: true, data: { tasks, nbHits: tasks.length } });
+    // res
+    //   .status(200)
+    //   .json({ status: 'success', data: { tasks, nbHits: tasks.length } });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
