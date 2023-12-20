98c3dd18a36e8c19f710290841d2df4fac8800bf
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-20 05:48:46 +0530

    add frontend

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,20 +5,11 @@ const connectDB = require('./db/connect');
 require('dotenv').config();
 
 // middleware
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-app.get('/', (req, res) => {
-  res.send('Task manager app');
-});
-
 app.use('/api/v1/tasks', tasks);
-
-// app.get('/api/v1/tasks')   - get all the tasks
-// app.post('/api/v1/tasks')   - create a new task
-// app.get('/api/v1/tasks/:id')   - get single task
-// app.patch('/api/v1/tasks/:id')   -update task
-// app.delete('/api/v1/tasks/:id')   -delete task
 
 const port = 3000;
 
