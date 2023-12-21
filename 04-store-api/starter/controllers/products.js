2b51cb20324f6440a9d81709a0f2987e4229ce1a
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-21 07:27:23 +0530

    company and name search

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,18 +1,28 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
+  const search = 'ab';
   const products = await Product.find({
-    featured: true,
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    // queryObject.name = name;
+    queryObject.name = { $regex: name, $options: 'i' };
   }
 
   console.log(queryObject);
