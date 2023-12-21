ae58fc43c60ebdcc21698ca42e9f80a6225693a0
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-21 21:39:24 +0530

    pagination setup

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -35,13 +35,21 @@ const getAllProducts = async (req, res) => {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
-    result = result.sort('createdAt');
+    result = result.sort('createdAt _id');
   }
 
   if (fields) {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  //23
+  //4 7 7 7 2
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
