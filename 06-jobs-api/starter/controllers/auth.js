db5ae928aee8ed9fa7a13e1dea40fe1681fc3ce6
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-23 20:51:49 +0530

    compare password

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -16,11 +16,17 @@ const login = async (req, res) => {
   }
 
   const user = await User.findOne({ email });
-  //compare password
 
   if (!user) {
     throw new UnauthenticatedError('Invalid credentials');
   }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+
+  // compare password
 
   const token = user.createJWT();
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
