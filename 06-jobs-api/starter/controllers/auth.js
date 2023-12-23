026577144c481c2ae207de8f04e6cf3f288785d6
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-23 19:02:04 +0530

    error checking controller example

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,9 +1,15 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
+  // checking in controller
+  //   const { name, email, password } = req.body;
+  //   if (!name || !email || !password) {
+  //     throw new BadRequestError('Please provide name,email and password');
+  //   }
+
   const user = await User.create({ ...req.body });
-  //   res.status(StatusCodes.CREATED).json(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
 
