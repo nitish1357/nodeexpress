39d2c485d4abde3d269e09a24802d35ae327ab16
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-24 13:40:16 +0530

    update user setup

--- 06.5-jobster-api/starter/routes/auth.js
+++ 06.5-jobster-api/starter/routes/auth.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
+const express = require('express');
+const router = express.Router();
+const authenticateUser = require('../middleware/authentication');
 
-module.exports = router
+const { register, login, updateUser } = require('../controllers/auth');
+router.post('/register', register);
+router.post('/login', login);
+router.patch('/updateUser', authenticateUser, updateUser);
+
+module.exports = router;
