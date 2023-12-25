4f3642f577bc47e8f74afacf9f52046900ef2580
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-25 22:00:18 +0530

    additional checks

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,10 +1,26 @@
 const path = require('path');
 
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
   //   console.log(req.files);
-  let productImage = req.files.image;
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file uploaded');
+  }
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please upload image');
+  }
+
+  const maxSize = 1000;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1KB'
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
