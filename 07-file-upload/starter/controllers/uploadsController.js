cd2f6f3ae039905273577d7028908c9be8700278
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-25 22:38:20 +0530

    remove temp files

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const cloudinary = require('cloudinary');
+const fs = require('fs');
 
 const uploadProductImageLocal = async (req, res) => {
   //   console.log(req.files);
@@ -42,6 +43,7 @@ const uploadProductImage = async (req, res) => {
       folder: 'file-upload',
     }
   );
+  fs.unlinkSync(req.files.image.tempFilePath);
   res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
