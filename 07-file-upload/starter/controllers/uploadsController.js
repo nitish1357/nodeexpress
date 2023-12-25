813d7e824a3dcb82126513b709add12e54bdc3f7
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-25 21:35:24 +0530

    express file upload package

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
 const { StatusCodes } = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
-  res.send('upload product image');
+  //   console.log(req.files);
+  let productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = { uploadProductImage };
