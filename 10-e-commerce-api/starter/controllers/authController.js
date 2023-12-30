aa427e68e4dfdd000dfbafce139b1d72b0df4092
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-30 06:35:31 +0530

    login route

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -24,6 +24,24 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password');
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+
+  attachCookiesToResponnse({ res, user: tokenUser });
+
   res.send('login user');
 };
 
