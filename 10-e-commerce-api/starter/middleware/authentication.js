201a098df00c8123057749cd8610e369d0e9d895
Nitish1357 <nitishmehta1990@gmail.com>
2023-12-31 20:38:16 +0530

    authorize permissions complete

--- 10-e-commerce-api/starter/middleware/authentication.js
+++ 10-e-commerce-api/starter/middleware/authentication.js
@@ -19,14 +19,15 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
-const authorizePermissions = (req, res, next) => {
-  //   console.log('admin route');
-  if (req.user.role !== 'admin') {
-    throw new CustomError.UnauthorizedError(
-      'Unauthorized to access this route'
-    );
-  }
-  next();
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
+    }
+    next();
+  };
 };
 
 module.exports = {
--- 10-e-commerce-api/starter/routes/userRoutes.js
+++ 10-e-commerce-api/starter/routes/userRoutes.js
@@ -13,7 +13,10 @@ const {
   updateUserPassword,
 } = require('../controllers/userController');
 
-router.route('/').get(authenticateUser, authorizePermissions, getAllUsers);
+router
+  .route('/')
+  .get(authenticateUser, authorizePermissions('admin'), getAllUsers);
+//   .get(authenticateUser, authorizePermissions('admin','owner'), getAllUsers);
 
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
