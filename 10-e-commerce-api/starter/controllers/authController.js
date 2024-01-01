36e3310ddd3571fa9f2bd8076dd7824c3ab7a9b0
Nitish1357 <nitishmehta1990@gmail.com>
2024-01-01 20:06:17 +0530

    createTokenUser function

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponnse } = require('../utils');
+const { attachCookiesToResponnse, createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -16,7 +16,7 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
 
   attachCookiesToResponnse({ res, user: tokenUser });
 
@@ -38,7 +38,7 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
 
   attachCookiesToResponnse({ res, user: tokenUser });
 
--- 10-e-commerce-api/starter/utils/index.js
+++ 10-e-commerce-api/starter/utils/index.js
@@ -1,3 +1,9 @@
 const { createJWT, isTokenValid, attachCookiesToResponnse } = require('./jwt');
+const createTokenUser = require('./createTokenUser');
 
-module.exports = { createJWT, isTokenValid, attachCookiesToResponnse };
+module.exports = {
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponnse,
+  createTokenUser,
+};
