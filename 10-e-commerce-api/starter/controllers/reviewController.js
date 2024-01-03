fe7b9739b35818016c5f6e97b2cfcef4038d0aa7
Nitish1357 <nitishmehta1990@gmail.com>
2024-01-03 07:24:10 +0530

    Review routes structure

---
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -0,0 +1,26 @@
+const createReview = async (req, res) => {
+  res.send('create review');
+};
+
+const getAllReviews = async (req, res) => {
+  res.send('get all reviews');
+};
+
+const getSingleReview = async (req, res) => {
+  res.send('get single review');
+};
+
+const updateReview = async (req, res) => {
+  res.send('update review');
+};
+const deleteReview = async (req, res) => {
+  res.send('delete review');
+};
+
+module.exports = {
+  getAllReviews,
+  getSingleReview,
+  createReview,
+  updateReview,
+  deleteReview,
+};
