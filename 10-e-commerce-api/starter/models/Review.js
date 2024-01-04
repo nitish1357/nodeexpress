1754e1e87b2c92ca752b37237b49bb99062885ad
Nitish1357 <nitishmehta1990@gmail.com>
2024-01-04 07:11:35 +0530

    aggregate pipeline setup

--- 10-e-commerce-api/starter/models/Review.js
+++ 10-e-commerce-api/starter/models/Review.js
@@ -33,4 +33,18 @@ const ReviewSchema = mongoose.Schema(
 );
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  console.log(productId);
+};
+
+ReviewSchema.post('save', async function () {
+  await this.constructor.calculateAverageRating(this.product);
+  //   console.log('post save hook called');
+});
+
+ReviewSchema.post('remove', async function () {
+  await this.constructor.calculateAverageRating(this.product);
+  //   console.log('post remove hook called');
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
